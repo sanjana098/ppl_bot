d968b6616a53db4bbed5f0f20672511a7be9efcc
SanjuBilly <sanjana22021998@gmail.com>
2017-02-05 14:16:34 +0530

    Bot can tell the current round no.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 'use strict'
 
+import http 	  from 'http';
 import express 	  from 'express';
 import bodyParser from 'body-parser';
 import request 	  from 'request';
 import apiai 	  from 'apiai';
 
 const app = express()
-const asd = apiai("3793e4868a974eb5aa8e810282c302fd");
+const Apiai = apiai("3793e4868a974eb5aa8e810282c302fd");
 
-app.set('port', (5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.set('port', 5000)
 
 // Process application/json
 app.use(bodyParser.json())
 
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
-
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
+app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
@@ -30,36 +23,74 @@ app.get('/webhook/', function (req, res) {
     res.send('Error, wrong token'+ req.query['hub.verify_token'])
 })
 
-// Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
-app.post('/webhook/', function (req, res) {
+app.post('/webhook', function (req, res) {      // For Facebook messenger
+
     let messaging_events = req.body.entry[0].messaging
+
     for (let i = 0; i < messaging_events.length; i++) {
+
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
+
         	if (event.message && event.message.text) {
+
         	    let text = event.message.text
-        	    var request = asd.textRequest(text, {
-        	    	sessionId: event.sender.id
-        	    });
-        	    request.on('response', function(response) {
-        	    	console.log(response,"***");
-    				sendTextMessage(sender,response.result.fulfillment.speech);
-				});
-				request.on('error', function(error) {
-				    sendTextMessage(sender, "Oops! Something went wrong...");
-				});
-				request.end();
+                getReply(sender, text);
+
         	}
     }
-    res.sendStatus(200)
+    res.sendStatus(200);
 })
 
+app.post('/pplBot', function (req, res) {       // For the PPL web app
+    var message = req.message.text;
+    var sender  = req.message.sender;
+
+    getReply(sender, message);
+
+    res.sendStatus(200);
+})
+
+function getReply(sender,text) {
+
+    var req = Apiai.textRequest(text, {
+                  sessionId: sender
+              });
+
+    req.on('response', function(response) {
+
+        if(response.result.metadata.intentName == 'round') {                    
+            request.post(
+                'http://localhost:8000/api/bot/getRoundNum',
+                {
+                    json: {
+                            message: 'round'
+                          }
+                },
+                function(error,response,body) {
+                    sendTextMessage(sender,body.message);
+                }
+            );
+        }
+        else {
+            sendTextMessage(sender,response.result.fulfillment.speech);
+        }
+    });
+    req.on('error', function(error) {
+        sendTextMessage(sender, "Oops! Something went wrong...");
+    });
+    req.end();
+
+}
+
 function sendTextMessage(sender, text) {
+
     let messageData = { text:text }
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
